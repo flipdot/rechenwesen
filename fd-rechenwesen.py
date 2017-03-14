66e2c8e3fb6910df0af0ffeb2a881a9d0c3ede53
Soeren Wegener <wegener92@gmail.com>
2017-03-14 23:00:02 +0100

    Init se rechenwesen.

---
+++ fd-rechenwesen.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
